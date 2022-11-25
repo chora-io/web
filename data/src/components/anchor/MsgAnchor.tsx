018b9652fabf8359c60e29716f3d1a8096e9ff07
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2022-11-25 00:25:55 -0800

    anchor tx progress

--- data/src/components/anchor/MsgAnchor.tsx
+++ data/src/components/anchor/MsgAnchor.tsx
@@ -1,12 +1,18 @@
 import * as React from "react"
 import { useState } from "react"
+import { Buffer } from "buffer"
 
-import { BroadcastMode, SignDoc } from "@keplr-wallet/types"
-import { AuthInfo, TxBody } from "@keplr-wallet/proto-types/cosmos/tx/v1beta1/tx"
+import { Coin } from "@keplr-wallet/proto-types/cosmos/base/v1beta1/coin"
+import { PubKey } from "@keplr-wallet/proto-types/cosmos/crypto/secp256k1/keys"
 import { SignMode } from "@keplr-wallet/proto-types/cosmos/tx/signing/v1beta1/signing"
+import { AuthInfo, Fee, TxBody, TxRaw } from "@keplr-wallet/proto-types/cosmos/tx/v1beta1/tx"
+import { Any } from "@keplr-wallet/proto-types/google/protobuf/any"
+import { BroadcastMode, SignDoc } from "@keplr-wallet/types"
 
 import { MsgAnchor } from "../../../api/regen/data/v1/tx"
 import {
+  ContentHash,
+  ContentHash_Graph,
   DigestAlgorithm,
   GraphCanonicalizationAlgorithm,
   GraphMerkleTree,
@@ -32,11 +38,11 @@ const MsgAnchorView = () => {
   const handleSubmit = (event: { preventDefault: () => void }) => {
     event.preventDefault()
 
+    let msg: MsgAnchor
     let encoder = new TextEncoder()
 
-    let msg: MsgAnchor
     if (type == "graph") {
-      msg = {
+      msg = MsgAnchor.fromJSON({
         $type: "regen.data.v1.MsgAnchor",
         sender: "regen1jx34255cgvxpthkg572ma6rhq6crwl6x2s4ajx", // TODO
         contentHash: {
@@ -49,9 +55,9 @@ const MsgAnchorView = () => {
             merkleTree: GraphMerkleTree.GRAPH_MERKLE_TREE_NONE_UNSPECIFIED, // TODO
           }
         },
-      }
+      })
     } else if (type == "raw") {
-      msg = {
+      msg = MsgAnchor.fromJSON({
         $type: "regen.data.v1.MsgAnchor",
         sender: "regen1jx34255cgvxpthkg572ma6rhq6crwl6x2s4ajx", // TODO
         contentHash: {
@@ -63,7 +69,7 @@ const MsgAnchorView = () => {
             mediaType: RawMediaType.UNRECOGNIZED, // TODO
           }
         },
-      }
+      })
     } else {
       setError("data type is required")
       return // exit on error
@@ -75,21 +81,31 @@ const MsgAnchorView = () => {
     // TODO: refactor connect wallet state
     const signer = "regen1jx34255cgvxpthkg572ma6rhq6crwl6x2s4ajx"
 
-    const txBody: TxBody = {
-      // @ts-ignore // TODO ?
-      messages: [msg],
-      memo: "test memo",
+    const msgValue = Buffer.from(JSON.stringify(msg))
+
+    const msgAny = Any.encode({
+      typeUrl: `/${msg.$type}`,
+      value: msgValue,
+    }).finish()
+
+    const msgAnyString = Buffer.from(JSON.stringify(msgAny)).toString('base64')
+
+    const txBody = TxBody.fromJSON({
+      messages: [
+        {
+          typeUrl: `/${msg.$type}`,
+          value: msgAnyString,
+        },
+      ],
+      memo: "test",
       timeoutHeight: "0",
-      extensionOptions: [],
-      nonCriticalExtensionOptions: []
-    }
+      extensionOptions: undefined,
+      nonCriticalExtensionOptions: undefined
+    })
 
-    // TODO: Request failed with status code 501 / Request rejected
-    // body_bytes is protobuf serialization of a TxBody that matches the
-    // representation in TxRaw.
-    const bodyBytes = new TextEncoder().encode(JSON.stringify(txBody))
+    const bodyBytes = TxBody.encode(txBody).finish()
 
-    const authInfo: AuthInfo = {
+    const authInfo = AuthInfo.fromJSON({
       signerInfos: [
         {
           publicKey: undefined,
@@ -97,57 +113,103 @@ const MsgAnchorView = () => {
             single: {
               mode: SignMode.SIGN_MODE_DIRECT
             },
-            multi: {
-              bitarray: undefined,
-              modeInfos: [],
-            }
+            multi: undefined,
           },
-          sequence: ""
+          sequence: "1"
         }
       ],
       fee: {
-        amount: [
-          {
-            denom: "uregen",
-            amount: "0"
-          }
-        ],
         gasLimit: "0",
         payer: "regen1jx34255cgvxpthkg572ma6rhq6crwl6x2s4ajx",
         granter: ""
       }
-    }
+    })
 
-    // TODO: Request failed with status code 501 / Request rejected
-    // auth_info_bytes is a protobuf serialization of an AuthInfo that matches the
-    // representation in TxRaw.
-    const authInfoBytes = new TextEncoder().encode(JSON.stringify(authInfo))
+    const authInfoBytes = AuthInfo.encode(authInfo).finish()
 
     // TODO: fetch account number
     const accountNumber = 1
 
     const signDoc: SignDoc = { bodyBytes, authInfoBytes, chainId, accountNumber }
 
-    window?.keplr?.signDirect(chainId, signer, signDoc).then(tx => {
-      setResponse(JSON.stringify(tx, null, "\t"))
+    window?.keplr?.signDirect(chainId, signer, signDoc).then(signResponse => {
+      console.log("sign success", signResponse)
 
-      // TODO: generate tx bytes
-      const txBytes =  new TextEncoder().encode(JSON.stringify(tx))
+      const signedTx = TxRaw.encode({
+        bodyBytes: TxBody.encode(
+          TxBody.fromPartial({
+            messages: [ // TODO
+              {
+                typeUrl: msg.$type,
+                value: MsgAnchor.encode({
+                  $type: "regen.data.v1.MsgAnchor",
+                  sender: "regen1jx34255cgvxpthkg572ma6rhq6crwl6x2s4ajx", // TODO
+                  contentHash: ContentHash.encode({
+                    $type: "regen.data.v1.ContentHash",
+                    graph: ContentHash_Graph.encode({
+                      $type: "regen.data.v1.ContentHash.Graph",
+                      hash:  encoder.encode(hash),
+                      digestAlgorithm:  DigestAlgorithm.DIGEST_ALGORITHM_BLAKE2B_256, // TODO
+                      canonicalizationAlgorithm: GraphCanonicalizationAlgorithm.GRAPH_CANONICALIZATION_ALGORITHM_URDNA2015, // TODO
+                      merkleTree: GraphMerkleTree.GRAPH_MERKLE_TREE_NONE_UNSPECIFIED, // TODO
+                    }).finish()
+                  }).finish(),
+                }).finish(),
+              },
+            ],
+            memo: "test", // signResponse.signed.memo,
+            extensionOptions: undefined,
+            nonCriticalExtensionOptions: undefined,
+          })
+        ).finish(),
+        authInfoBytes: AuthInfo.encode({
+          signerInfos: [
+            {
+              publicKey: {
+                typeUrl: "/cosmos.crypto.secp256k1.PubKey",
+                value: PubKey.encode({
+                  key: Buffer.from(signResponse.signature.pub_key.value, "base64"),
+                }).finish(),
+              },
+              modeInfo: {
+                single: {
+                  mode: SignMode.SIGN_MODE_DIRECT,
+                },
+                multi: undefined,
+              },
+              sequence: "1", // signResponse.signed.sequence,
+            },
+          ],
+          fee: Fee.fromPartial({
+            amount: Coin.encode({
+              denom: "uregen",
+              amount: "0",
+            }).finish(), // signResponse.signed.fee.amount as Coin[],
+            gasLimit: "0", // signResponse.signed.fee.gas,
+            payer: "regen1jx34255cgvxpthkg572ma6rhq6crwl6x2s4ajx", // signResponse.signed.fee["feePayer"]
+          }),
+        }).finish(),
+        signatures: [
+            Buffer.from(signResponse.signature.signature, "base64"),
+        ],
+      }).finish()
 
-      // TODO: choose broadcast mode
+      console.log("signedTx", signedTx)
+
       const mode = "sync" as BroadcastMode
 
-      window?.keplr?.sendTx(chainId, txBytes, mode).then(res => {
-        console.log("send tx success", res)
+      window?.keplr?.sendTx(chainId, signedTx, mode).then(res => {
+        console.log("send success", res)
         setResponse(JSON.stringify(res, null, "\t"))
+
       }).catch(err => {
-        console.log(err.message)
-        setError(err.message)
+        console.log("send error", err.message)
+        setError("send error: " + err.message)
       })
 
     }).catch(err => {
-      console.log(err.message)
-      setError(err.message)
+      console.log("sign error", err.message)
+      setError("sign error: " + err.message)
     })
   }
 
