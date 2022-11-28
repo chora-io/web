b4caebc20e3dba3b917bc295c2c461e987617e0b
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2022-11-27 22:21:00 -0800

    fix back link

--- scan/src/components/dash/Dashboard.tsx
+++ scan/src/components/dash/Dashboard.tsx
@@ -5,13 +5,7 @@ import * as styles from "./Dashboard.module.css"
 
 const Dashboard = ({ name }: any) => {
 
-  let local = false
-  if (typeof window !== "undefined" && (
-      window.location.hostname == "0.0.0.0" ||
-      window.location.hostname == "127.0.0.1" ||
-      window.location.hostname == "localhost"
-    )
-  ) { local = true }
+  // ...
 
   return (
     <div className={styles.container}>
@@ -19,7 +13,7 @@ const Dashboard = ({ name }: any) => {
         {name + " Dashboard"}
       </div>
       <div>
-        <Link to={local ? "/" : "/scan"}>
+        <Link to={"/"}>
           {"back"}
         </Link>
       </div>
