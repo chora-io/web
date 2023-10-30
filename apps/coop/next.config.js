05454b714b3b6ec35572e387c77e0a725163528b
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2023-10-29 18:14:57 -0700

    fix next config

--- apps/coop/next.config.js
+++ apps/coop/next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  basePath: '',
-  transpilePackages: ['chora'],
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+const nextConfig = (phase) => {
+  return {
+    basePath: '',
+    transpilePackages: ['chora'],
+  }
 }
 
 module.exports = nextConfig
--- apps/scan/next.config.js
+++ apps/scan/next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  basePath: '',
-  transpilePackages: ['chora'],
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+const nextConfig = (phase) => {
+  return {
+    basePath: phase === PHASE_DEVELOPMENT_SERVER ? '' : '/scan',
+    transpilePackages: ['chora'],
+  }
 }
 
 module.exports = nextConfig
