8ed11ac678c5ecd06137f71cad24b3e68786948c
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2024-03-21 10:05:37 -0700

    beta application info

--- apps/coop/app/page.tsx
+++ apps/coop/app/page.tsx
@@ -9,7 +9,25 @@ export const metadata: Metadata = {
 const HomePage = () => (
   <div className={styles.page}>
     <div>
-      <h1>{'group governance workspace'}</h1>
+      <h1>{'chora coop'}</h1>
+      <h2>{'group governance workspace'}</h2>
+      <p>
+        {'This application is a '}
+        <span style={{ fontWeight: '400', textDecoration: 'underline' }}>
+          {'beta application'}
+        </span>
+        {'. This application is not ready for production use.'}
+      </p>
+      <p>
+        {
+          'Limiting your interactions with main networks is recommended until the application matures.'
+        }
+      </p>
+      <p>
+        {
+          'Thank you for testing out the application. Please enjoy and use with care and caution.'
+        }
+      </p>
     </div>
   </div>
 )
